adb65de8ddd4bb7d00d46417a57a1409a4e7d4ce
nikki0210x <nikki0210x@gmail.com>
2025-11-24 01:23:42 +0900

    修正

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -22,11 +22,10 @@ export default function Sidebar() {
 	const [isOpen, setIsOpen] = useState(false);
 	const [isMobile, setIsMobile] = useState(false);
 	const [isSmallScreen, setIsSmallScreen] = useState(false);
-
 	useEffect(() => {
 		const checkScreens = () => {
 			setIsMobile(window.innerWidth < 1024);
-			setIsSmallScreen(window.innerWidth < 448)
+			setIsSmallScreen(window.innerWidth < 448);
 		};
 		checkScreens();
 		window.addEventListener("resize", checkScreens);
@@ -68,7 +67,7 @@ export default function Sidebar() {
 				isIconOnly
 				size="lg"
 				radius="full"
-				className={`fixed top-0 left-0 z-100 w-16 h-16 rounded-none transition-all duration-500 outline-none bg-transparent hover:bg-l4 hover:dark:bg-d4 active:bg-l3 active:dark:bg-d3 focus-visible:bg-l4 focus-visible:dark:bg-d4 text-d2 dark:text-l2 ${isOpen ? "rounded-br-4xl" : "rounded-br-none"}`}
+				className={`fixed top-0 left-0 z-100 w-16 h-16 rounded-none transition-all duration-500 outline-none bg-transparent hover:bg-l4 hover:dark:bg-d4 active:bg-l3 active:dark:bg-d3 focus-visible:bg-l4 focus-visible:dark:bg-d4 text-d2 dark:text-l2 ${isOpen || isMobile ? "rounded-br-4xl" : "rounded-br-none"}`}
 				onPress={() => setIsOpen(!isOpen)}
 			>
 				<Menu />
@@ -84,45 +83,42 @@ export default function Sidebar() {
 				<SquarePen />
 			</Button>
 			<AnimatePresence>
-				{(isOpen || !isMobile) && (
-					<>
-						{isMobile && isOpen && (
-							<motion.div
-								key="overlay"
-								initial={{ opacity: 0 }}
-								animate={{ opacity: 1 }}
-								exit={{ opacity: 0 }}
-								transition={{ duration: 0.5, ease: easeInOut }}
-								onClick={() => setIsOpen(false)}
-								className="fixed top-0 left-0 z-80 w-full h-full backdrop-blur-[2px] bg-l1/50 dark:bg-d1/50"
-							/>
-						)}
-						<motion.aside
-							key="sidebar"
-							initial={{ width: isMobile ? 0 : "4rem" }}
-							animate={{
-								width: isOpen
-									? (isMobile ? "min(calc(100vw - 4rem), 24rem)" : "24rem")
-									: "4rem"
-							}} exit={{ width: 0 }}
-							transition={{ duration: 0.5, ease: easeInOut }}
-							className={`flex flex-col w-auto h-full overflow-hidden bg-l2 dark:bg-d2 ${isMobile ? "fixed top-0 left-0 z-90" : "relative"}`}
-						>
-							<div className="flex flex-row justify-between items-center w-full h-16">
-								<div className="overflow-hidden shrink-0 w-16 h-full"></div>
-								<Button aria-label="Account Button" className={`overflow-hidden flex flex-row gap-4 justify-start items-center w-auto h-full rounded-none transition-all duration-500 outline-none bg-l3 dark:bg-d3 hover:bg-l4 hover:dark:bg-d4 active:bg-l3 active:dark:bg-d3 focus-visible:bg-l4 focus-visible:dark:bg-d4 ${isOpen ? "rounded-bl-4xl" : "rounded-bl-none"}`}>
-									<CircleUserRound className="p-0.5 w-16 h-16 shrink-0" />
-									<span className="text-lg font-medium text-d2 dark:text-l2">ログイン</span>
-								</Button>
-							</div>
-							<div className="flex flex-col justify-start items-start flex-1 w-full h-full"></div>
-							<Button aria-label="Settings Button" className="overflow-hidden flex flex-row gap-4 justify-start items-center w-full h-16 rounded-none outline-none bg-transparent hover:bg-l4 hover:dark:bg-d4 active:bg-l3 active:dark:bg-d3 focus-visible:bg-l4 focus-visible:dark:bg-d4">
-								<Settings className="p-0.5 w-16 h-16 shrink-0" />
-								<span className="text-lg font-medium text-d2 dark:text-l2">設定</span>
-							</Button>
-						</motion.aside>
-					</>
+				{isOpen && isMobile && (
+					<motion.div
+						key="overlay"
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+						transition={{ duration: 0.5, ease: easeInOut }}
+						onClick={() => setIsOpen(false)}
+						className="fixed top-0 left-0 z-80 w-full h-full backdrop-blur-[2px] bg-l1/50 dark:bg-d1/50"
+					/>
 				)}
+				<motion.aside
+					key={`sidebar-${isMobile}`}
+					initial={false}
+					animate={{
+						width: isOpen
+							? (isMobile ? "min(calc(100vw - 4rem), 24rem)" : "24rem")
+							: isMobile ? 0 : "4rem"
+					}}
+					exit={{ width: isMobile ? 0 : "4rem" }}
+					transition={{ duration: 0.5, ease: easeInOut }}
+					className={`flex flex-col w-auto h-full overflow-hidden bg-l2 dark:bg-d2 ${isMobile ? "fixed top-0 left-0 z-90" : "relative"}`}
+				>
+					<div className="flex flex-row justify-between items-center w-full h-16">
+						<div className="overflow-hidden shrink-0 w-16 h-full"></div>
+						<Button aria-label="Account Button" className={`overflow-hidden flex flex-row gap-4 justify-start items-center w-auto h-full rounded-none transition-all duration-500 outline-none bg-l3 dark:bg-d3 hover:bg-l4 hover:dark:bg-d4 active:bg-l3 active:dark:bg-d3 focus-visible:bg-l4 focus-visible:dark:bg-d4 ${isOpen || isMobile ? "rounded-bl-4xl" : "rounded-bl-none"}`}>
+							<CircleUserRound className="p-0.5 w-16 h-16 shrink-0" />
+							<span className="text-lg font-medium text-d2 dark:text-l2">ログイン</span>
+						</Button>
+					</div>
+					<div className="flex flex-col justify-start items-start flex-1 w-full h-full"></div>
+					<Button aria-label="Settings Button" className="overflow-hidden flex flex-row gap-4 justify-start items-center w-full h-16 rounded-none outline-none bg-transparent hover:bg-l4 hover:dark:bg-d4 active:bg-l3 active:dark:bg-d3 focus-visible:bg-l4 focus-visible:dark:bg-d4">
+						<Settings className="p-0.5 w-16 h-16 shrink-0" />
+						<span className="text-lg font-medium text-d2 dark:text-l2">設定</span>
+					</Button>
+				</motion.aside>
 			</AnimatePresence>
 		</div >
 	);
