0b582a60847017e88d7f06a452a6ed4379058848
nikki0210x <nikki0210x@gmail.com>
2025-12-20 22:55:18 +0900

    修正

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,7 @@
 /* src\lib\prisma.ts */
 
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const connectionString = process.env.DATABASE_URL;
 
