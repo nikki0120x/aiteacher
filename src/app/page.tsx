d189974dabf9c62af9c2f2d325fa02da4f156f16
nikki0210x <nikki0210x@gmail.com>
2025-12-20 23:03:37 +0900

    修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,11 @@
 /* src\app\page.tsx */
 "use client";
-import {
-	Accordion,
-	AccordionItem,
-	Button,
-	Card,
-	CardBody,
-	Divider,
-	Dropdown,
-	DropdownItem,
-	DropdownMenu,
-	DropdownTrigger,
-	ScrollShadow,
-	Slider,
-	Switch,
-	Textarea,
-	Tooltip,
-} from "@heroui/react";
+import { Switch } from "@heroui/react";
 
 export default function Chat() {
-	return <div className="flex size-full">
-		<Switch
-		>
-			指針
-		</Switch>
-	</div>;
+	return (
+		<div className="flex size-full">
+			<Switch>指針</Switch>
+		</div>
+	);
 }
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,8 @@
-/* src\lib\prisma.ts */
+/* src/lib/prisma.ts */
 
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
 
 const connectionString = process.env.DATABASE_URL;
 
@@ -9,12 +10,12 @@ if (!connectionString) {
 	throw new Error("DATABASE_URL is not set in environment variables");
 }
 
-const adapter = new PrismaPg({
-	connectionString,
-});
+const pool = new Pool({ connectionString });
+
+const adapter = new PrismaPg(pool);
 
 const globalForPrisma = global as unknown as {
-	prisma: PrismaClient;
+	prisma: PrismaClient | undefined;
 };
 
 const prisma =
