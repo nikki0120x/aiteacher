04edac1ae7abe6d032fe5fc5fa3e4bad90ca9640
nikki0210x <nikki0210x@gmail.com>
2026-01-03 00:33:56 +0900

    修正

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -8,17 +8,17 @@ export default function manifest(): MetadataRoute.Manifest {
         description: "勉強に浪漫と好奇心を。そして、智慧と伴に未だ見ぬ未来を切り拓け！",
         start_url: "/",
         display: "standalone",
-        background_color: "#0d0d0d",
+        background_color: "#f2f2f2",
         theme_color: "#00bfff",
         icons: [
             {
-                src: "/images/icons/png/Icon_AITeacher_small_theme.png",
+                src: "/images/apps/png/App_AITeacher_small_transparent.png",
                 sizes: "500x500",
                 type: "image/png",
                 purpose: "any",
             },
             {
-                src: "/images/icons/png/Icon_AITeacher_large_theme.png",
+                src: "/images/apps/png/App_AITeacher_large_transparent.png",
                 sizes: "1000x1000",
                 type: "image/png",
                 purpose: "any",
