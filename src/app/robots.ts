6a1e77a5d588677c7cfe3508721023ffa26bb702
nikki0210x <nikki0210x@gmail.com>
2026-01-03 11:33:21 +0900

    修正

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -11,6 +11,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: isProduction ? "/" : undefined,
             disallow: isProduction ? "/api/" : "/",
         },
-        sitemap: isProduction ? `${baseUrl}/sitemap.xml` : undefined,
+        sitemap: isProduction ? `${baseUrl}sitemap.xml` : undefined,
     };
 }
